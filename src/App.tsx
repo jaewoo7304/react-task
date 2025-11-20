7096c109ad53dd1b8c21207443d5ffd012ca46fa
jaewoo7304 <germano001001@gmail.com>
2025-11-20 17:42:38 +0900

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,121 @@
-import { appContainer, board, buttons } from "./App.css"
-import BoardList from "./components/BoardList/BoardList"
+import { useState } from "react";
+import {
+  appContainer,
+  board,
+  buttons,
+  deleteBoardButton,
+  loggerButton,
+} from "./App.css";
+import BoardList from "./components/BoardList/BoardList";
+import ListsContainer from "./components/ListsContainer/ListsContainer";
+import { useTypedDispatch, useTypedSelector } from "./hooks/redux";
+import EditModal from "./components/EditModal/EditModal";
+import LoggerModal from "./components/LoggerModal/LoggerModal";
+import { deleteBoard, sort } from "./store/slices/boardsSlice";
+import { addLog } from "./store/slices/loggerSlice";
+import { v4 } from "uuid";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 function App() {
-  const [activeBoardId, setActiveBoardId] =  useState('board-0');
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
+  const [activeBoardId, setActiveBoardId] = useState("board-0");
+  const { modalActive } = useTypedSelector((state) => state.boards);
+  const boards = useTypedSelector((state) => state.boards.boardArray);
+  const getActiveBoard = boards.filter(
+    (board) => board.boardId === activeBoardId
+  )[0];
+  const lists = getActiveBoard.lists;
+  const dispatch = useTypedDispatch();
+
+  const handleDeleteBoard = () => {
+    if (boards.length > 1) {
+      dispatch(deleteBoard({ boardId: getActiveBoard.boardId }));
+      dispatch(
+        addLog({
+          logId: v4(),
+          logMessage: `게시판 삭제하기: ${getActiveBoard.boardName}`,
+          logAuthor: "User",
+          logTimestamp: String(Date.now()),
+        })
+      );
+      const newIndexToSet = () => {
+        const indexToBeDeleted = boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        );
+        return indexToBeDeleted === 0
+          ? indexToBeDeleted + 1
+          : indexToBeDeleted - 1;
+      };
+
+      setActiveBoardId(boards[newIndexToSet()].boardId);
+    } else {
+      alert("최소 게시판 갯수는 1개 입니다.");
+    }
+  };
+  const handleDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) return;
+
+    const sourceList = lists.filter(
+      (list) => list.listId === source.droppableId
+    )[0];
+
+    dispatch(
+      sort({
+        boardIndex: boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        ),
+        droppableIdStart: source.droppableId,
+        droppableIdEnd: destination.droppableId,
+        droppableIndexStart: source.index,
+        droppableIndexEnd: destination.index,
+        draggableId,
+      })
+    );
+    dispatch(
+      addLog({
+        logId: v4(),
+        logAuthor: "User",
+        logMessage: `리스트"${sourceList.listName}"에서
+      리스트"${
+        lists.filter((list) => list.listId === destination.droppableId)[0]
+          .listName
+      }"으로
+      ${
+        sourceList.tasks.filter((task) => task.taskId === draggableId)[0]
+          .taskName
+      }을 옮김.`,
+        logTimestamp: String(Date.now()),
+      })
+    );
+  };
 
   return (
-      <div className={appContainer}>
-        <div className={board}>
-          <BoardList
-            activeBoardId={activeBoardId}
-            setActiveBoardId={setActiveBoardId}
-          />
-        </div>
-        <div className={buttons}>
-          <button>이 게시판 삭제하기</button>
-          <button></button>
-        </div>
+    <div className={appContainer}>
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
+      {modalActive ? <EditModal /> : null}
+      <BoardList
+        activeBoardId={activeBoardId}
+        setActiveBoardId={setActiveBoardId}
+      />
+      <div className={board}>
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+        </DragDropContext>
       </div>
-  )
+      <div className={buttons}>
+        <button className={deleteBoardButton} onClick={handleDeleteBoard}>
+          게시판 삭제
+        </button>
+        <button
+          className={loggerButton}
+          onClick={() => setIsLoggerOpen(!isLoggerOpen)}
+        >
+          {isLoggerOpen ? "활동 목록 숨기기" : "활동 목록 보기"}
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
